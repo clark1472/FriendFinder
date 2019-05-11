f99d638fc7355d1eca0ab69e04572de1578c5869
Salsa <>
2019-05-10 22:13:05 -0700

    make friends.js and images.jpg files, make images folder

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Dave Clark",
+        photo: "/images.DaveClark.jpg",
+        scores: [5, 2, 4, 4, 5, 3, 2, 5, 4, 4]
+    },
+    {
+        name: "Joe Smith",
+        photo: "/images.Brandon-0.jpg",
+        scores: [4, 1, 3, 1, 3, 1, 2, 1, 4, 5]
+    },
+    {
+        name: "Jeff Simmons",
+        photo: "/images.bradley-cooper.jpg",
+        scores: [3, 2, 4, 4, 2, 4, 2, 5, 3, 1]
+    },
+    {
+        name: "Mark Harmon",
+        photo: "/images.MarkHarmon.jpg",
+        scores: [2, 3, 5, 1, 2, 1, 2, 4, 4, 2]
+    },
+    {
+        name: "Harrison Ford",
+        photo: "/images.HarrisonFord.jpg",
+        scores: [1, 1, 1, 5, 5, 5, 3, 5, 3, 3]
+    },
+];
+
+module.exports = friends;
