082b6aa4546900f754999843fcfdc01b653c2739
Salsa <>
2019-05-12 22:40:23 -0700

    tweeked both html pages. Make both routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,44 @@
+// LOAD DATA
+var friends = require("../data/friends.js");
+
+// ROUTING
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    var user = req.body;
+
+    for (var i = 0; i < user.score.length; i++) {
+        user.score[i] = parseInt(user.scores[i]);
+        }
+        //The friend with the score closest to 0, (minimum difference) matches the user as best friend. If choices are 5-1=4 4*10 = 40 questions. That would be total opposite.
+        var bestfriendScore = 0;
+        var minimumDifference = 40;
+
+        //compares user with
+    for (var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+        for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores [j] - friends[i].scores[j]);
+        totalDifference += difference;
+        }
+    
+        if (totalDifference < minimumDifference) {
+            bestFriendIndex = i;
+            minimumDifference = totalDifference;
+        }
+    }   
+        friends.push(user);
+        res.json(friends[bestFriendIndex]);
+
+    });
+};
+    
+    
+  
+
+ 
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-Parser");
 
 // Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Data parsing Express app 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+// parse some custom thing into a Buffer. found in git expressjs/body-parser
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // The API and HTML routes
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 //confirmation server is listening
-app.listen(PORT, () => console.log("listening on port %s", PORT));
-//app.listen(PORT, function() {
-  //console.log("Listening on PORT: " + PORT);
-//});
+app.listen(PORT, function() {
+  console.log("Listening on PORT: " + PORT);
+});
 
 
 
